ddf6d034a1df818c0b67bcf7c12af0088304685f
Unknown_British_Man <matthewdesantis745@gmail.com>
2024-12-18 11:50:13 -0500

    changed folder name

--- products.js
+++ products.js
@@ -5,7 +5,7 @@ const products = [
       company: 'ikea',
       image: 'https://www.course-api.com/images/store/product-1.jpeg',
       price: 9.99,
-      link: 'highbackbench.html'
+      link: 'html-files/highbackbench.html'
     },
     {
       id: 'rec4f2RIftFCb7aHh',
@@ -13,7 +13,7 @@ const products = [
       company: 'marcos',
       image: 'https://www.course-api.com/images/store/product-2.jpeg',
       price: 79.99,
-      link: 'blackcouch.html'
+      link: 'html-files/blackcouch.html'
     },
     {
       id: 'rec8kkCmSiMkbkiko',
@@ -21,7 +21,7 @@ const products = [
       company: 'caressa',
       image: 'https://www.course-api.com/images/store/product-3.jpeg',
       price: 25.99,
-      link: 'accentchairs.html'
+      link: 'html-files/accentchairs.html'
     },
     {
       id: 'recBohCqQsot4Q4II',
@@ -29,7 +29,7 @@ const products = [
       company: 'caressa',
       image: 'https://www.course-api.com/images/store/product-4.jpeg',
       price: 45.99,
-      link: 'woodentable&chairs.html'
+      link: 'html-files/woodentable&chairs.html'
     },
     {
       id: 'recDG1JRZnbpRHpoy',
@@ -37,7 +37,7 @@ const products = [
       company: 'caressa',
       image: 'https://www.course-api.com/images/store/product-5.jpeg',
       price: 6.99,
-      link: 'diningtable.html'
+      link: 'html-files/diningtable.html'
     },
     {
       id: 'recNWGyP7kjFhSqw3',
@@ -45,7 +45,7 @@ const products = [
       company: 'liddy',
       image: 'https://www.course-api.com/images/store/product-6.jpeg',
       price: 69.99,
-      link: 'hoveringbed.html'
+      link: 'html-files/hoveringbed.html'
     },
     {
       id: 'recZEougL5bbY4AEx',
@@ -53,7 +53,7 @@ const products = [
       company: 'marcos',
       image: 'https://www.course-api.com/images/store/product-7.jpeg',
       price: 8.99,
-      link: 'modernbookshelf.html'
+      link: 'html-files/modernbookshelf.html'
     },
     {
       id: 'recjMK1jgTb2ld7sv',
@@ -61,7 +61,7 @@ const products = [
       company: 'liddy',
       image: 'https://www.course-api.com/images/store/product-8.jpeg',
       price: 21.99,
-      link: 'matchingchairset.html'
+      link: 'html-files/matchingchairset.html'
     },
     {
       id: 'recmg2a1ctaEJNZhu',
@@ -69,7 +69,7 @@ const products = [
       company: 'marcos',
       image: 'https://www.course-api.com/images/store/product-9.jpeg',
       price: 39.99,
-      link: 'longbrownchairs.html'
+      link: 'html-files/longbrownchairs.html'
     },
     {
       id: 'recvKMNR3YFw0bEt3',
@@ -77,7 +77,7 @@ const products = [
       company: 'liddy',
       image: 'https://www.course-api.com/images/store/product-10.jpeg',
       price: 29.99,
-      link: 'leatherloveseat.html'
+      link: 'html-files/leatherloveseat.html'
     },
     {
       id: 'recxaXFy5IW539sgM',
@@ -85,7 +85,7 @@ const products = [
       company: 'ikea',
       image: 'https://www.course-api.com/images/store/product-11.jpeg',
       price: 10.99,
-      link: 'dresser.html'
+      link: 'html-files/dresser.html'
     },
     {
       id: 'recyqtRglGNGtO4Q5',
@@ -93,6 +93,6 @@ const products = [
       company: 'liddy',
       image: 'https://www.course-api.com/images/store/product-12.jpeg',
       price: 9.99,
-      link: 'painting.html'
+      link: 'html-files/painting.html'
     },
   ];
