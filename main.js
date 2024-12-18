83d5977518d9070736236ea573a17e06c48c20c8
Unknown_British_Man <matthewdesantis745@gmail.com>
2024-12-18 11:19:06 -0500

    files

---
+++ main.js
@@ -0,0 +1,77 @@
+console.log(products)
+let filteredProducts = [...products]
+console.log(filteredProducts)
+
+//SELECTORS
+const productsContainer = document.querySelector(".not-sidebar")
+const searchForm = document.querySelector(".input-form")
+const searchInput = document.querySelector(".search-input")
+
+searchForm.addEventListener("keyup", () => {
+    let searchTerm = searchInput.value .toLowerCase();
+    console.log(searchTerm)
+    filteredProducts = products.filter(product => {
+        return product.title.includes(searchTerm)
+    })
+    displayProducts()
+})
+
+// Loop and display each product
+function displayProducts() {
+    productsContainer.innerHTML = filteredProducts.map(product => {
+       return `<article class="product">
+                    <img src="${product.image}" alt="" />
+                    <footer>
+                        <button class="product-name">${product.title}</button>
+                        <p class="product-price">${product.price}</p>
+                    </footer>
+                </article>`
+    }) .join("")
+}
+displayProducts()
+
+//Making the button do something
+const productName = document.querySelectorAll(".product-name")
+
+productName.forEach(items => {
+    items.addEventListener("click", myfunction)
+})
+
+function myfunction() {
+    console.log("It worked Big Boi")
+}
+
+//popup stuff
+document.addEventListener('DOMContentLoaded', function () {
+    const popupOverlay = document.getElementById('popupOverlay');
+    const popup = document.getElementById('popup');
+    const closePopup = document.getElementById('closePopup');
+    const emailInput = document.getElementById('Phonenumber');
+    const finish = document.getElementById('finish')
+  
+    function openPopup() {
+        popupOverlay.style.display = 'block';
+    }
+
+    function closePopupFunc() {
+        popupOverlay.style.display = 'none';
+    }
+
+   finish.addEventListener('click', function() {
+ const email = Phonenumber.value;
+        console.log(`Number submitted: ${email}`);
+        closePopupFunc(); 
+   })
+    
+    openPopup();
+
+    closePopup.addEventListener('click', closePopupFunc);
+
+    popupOverlay.addEventListener('click', function (event) {
+        if (event.target === popupOverlay) {
+            closePopupFunc();
+        }
+    });
+
+
+});
---
+++ products.js
@@ -0,0 +1,88 @@
+const products = [
+    {
+      id: 'rec43w3ipXvP28vog',
+      title: 'high-back bench',
+      company: 'ikea',
+      image: 'https://www.course-api.com/images/store/product-1.jpeg',
+      price: 9.99,
+    },
+    {
+      id: 'rec4f2RIftFCb7aHh',
+      title: 'Black Couch',
+      company: 'marcos',
+      image: 'https://www.course-api.com/images/store/product-2.jpeg',
+      price: 79.99,
+    },
+    {
+      id: 'rec8kkCmSiMkbkiko',
+      title: 'accent chair',
+      company: 'caressa',
+      image: 'https://www.course-api.com/images/store/product-3.jpeg',
+      price: 25.99,
+    },
+    {
+      id: 'recBohCqQsot4Q4II',
+      title: 'wooden table with chairs',
+      company: 'caressa',
+      image: 'https://www.course-api.com/images/store/product-4.jpeg',
+  
+      price: 45.99,
+    },
+    {
+      id: 'recDG1JRZnbpRHpoy',
+      title: 'dining table',
+      company: 'caressa',
+      image: 'https://www.course-api.com/images/store/product-5.jpeg',
+  
+      price: 6.99,
+    },
+    {
+      id: 'recNWGyP7kjFhSqw3',
+      title: 'Hovering bed',
+      company: 'liddy',
+      image: 'https://www.course-api.com/images/store/product-6.jpeg',
+      price: 69.99,
+    },
+    {
+      id: 'recZEougL5bbY4AEx',
+      title: 'modern bookshelf',
+      company: 'marcos',
+      image: 'https://www.course-api.com/images/store/product-7.jpeg',
+      price: 8.99,
+    },
+    {
+      id: 'recjMK1jgTb2ld7sv',
+      title: 'Matching Chair Set',
+      company: 'liddy',
+      image: 'https://www.course-api.com/images/store/product-8.jpeg',
+      price: 21.99,
+    },
+    {
+      id: 'recmg2a1ctaEJNZhu',
+      title: 'Long Brown Chairs',
+      company: 'marcos',
+      image: 'https://www.course-api.com/images/store/product-9.jpeg',
+      price: 39.99,
+    },
+    {
+      id: 'recvKMNR3YFw0bEt3',
+      title: 'Leather Love Seat',
+      company: 'liddy',
+      image: 'https://www.course-api.com/images/store/product-10.jpeg',
+      price: 29.99,
+    },
+    {
+      id: 'recxaXFy5IW539sgM',
+      title: 'Dresser',
+      company: 'ikea',
+      image: 'https://www.course-api.com/images/store/product-11.jpeg',
+      price: 10.99,
+    },
+    {
+      id: 'recyqtRglGNGtO4Q5',
+      title: 'paintings with a mattress on the ground',
+      company: 'liddy',
+      image: 'https://www.course-api.com/images/store/product-12.jpeg',
+      price: 9.99,
+    },
+  ];
